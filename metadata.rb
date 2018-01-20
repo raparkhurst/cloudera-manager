91650bf78558a90d1260a3a6978f69c24e668281
Robert Parkhurst <raparkhurst@digitalsynapse.io>
2018-01-20 00:24:12 -0600

    updating license file

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'cloudera-manager'
 maintainer       'Robert Parkhurst'
 maintainer_email 'raparkhurst@digitalsynapse.io'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures cloudera-manager'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.4'
+version          '0.2.5'
 
 
 depends "users"
